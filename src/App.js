af784e192945aace4bbf1c1268c4302eab1785b3
chavda-mitul <20dce020@charusat.edu.in>
2023-05-23 09:17:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { AdminPage } from './AdminPage';
 import './App.css';
+import HomePage from './HomePage';
+import {Route, Link, BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <BrowserRouter>
+    <div>
+      <Link to = "/">HomePage</Link>
+      <Link to = "/admin">AdminPage</Link>
+      
     </div>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+      <Route path='/admin' element={<AdminPage/>}/>
+    </Routes>
+
+    </BrowserRouter>
+   
+    </>
   );
 }
 
 export default App;
+// create a button to submit form
+// get name in console in when pressed button
+// set default value in the hooks
+//  
